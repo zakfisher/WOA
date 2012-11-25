94e20b71bd190b704fac0ed770ccc2a0e41b3842
Zak Fisher <zak.fisher@gmail.com>
2012-11-25 00:24:24 -0800

    google analytics

--- view/themes/default/templates/global/footer.php
+++ view/themes/default/templates/global/footer.php
@@ -39,7 +39,16 @@
 <?php foreach ($js as $path): ?>
 <script type='text/javascript' src='<?= $js_path . $path . '.js'; ?>'></script>
 <?php endforeach; ?>
+<?php if (ENV == 'www'): ?>
 <script type="text/javascript">
-    // Google Analytics
+   var _gaq = _gaq || [];
+   _gaq.push(['_setAccount', 'UA-15462493-1']);
+   _gaq.push(['_trackPageview']);
+   (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+   })();
 </script>
+<?php endif; ?>
 </html>
