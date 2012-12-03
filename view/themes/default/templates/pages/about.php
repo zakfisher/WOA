cba89f9a352cfd58fa91387ab70a940f14dc3b60
Zak Fisher <zak.fisher@gmail.com>
2012-12-03 14:41:39 -0800

    styled about us page

--- view/themes/default/templates/pages/about.php
+++ view/themes/default/templates/pages/about.php
@@ -21,24 +21,24 @@
             <div class="inner-container">
                <div class="header">
                   <h1>About Us</h1>
-                  <h2>What is the World of Anarchy?</h2>
+                  <h2>Company Overview</h2>
                   <div class="shadow down"></div>
                   <div class="dynamic-shell">
                      <div class="dynamic-content">
                         <div class="main-view">
                            <div class="content" data-sub-page="overview">
-                               <h1>Company Overview</h1>
-                               <div class="section">
-                                   <h3>What is WOA?</h3>
-                                   <p>Founded in 2008, World of Anarchy began as an event production company, dedicated to the growth of electronic music on a global scale.  By teaming up with industry leaders and charities like the AVON Walk for Breast Cancer and the Costanoan Ohlone Indian Research non-profit, WOA was able to take its first steps by producing community-benefitting events.</p>
-                                   <p>In August of 2009, World of Anarchy made their presence known with one of the most memorable events of the year: “Down The Rabbit Hole 2”.  An event that started off as a small idea rapidly grew into a 3 stage, 16 hour, 3,000 person festival, at a Federal Native American Trust Land.  The event fused Native American culture with electronic music culture, and left those that attended in sheer awe!  Those lucky enough to attend still enthusiastically rave about the event to this day.</p>
-                                   <p>In early 2010, plans for the “Return of the Hatter: Down the Rabbit Hole 3” were being hatched.  In August of 2011, WOA brought DTRH back to life at the Monterey County Fair Grounds.  DTRH 3 recreated the Native American - EDM fusion, only on a much larger scale; featuring 6 Massive stages, on-site camping, over 70 renowned DJs from all over the world, and 36 hours of non-stop music. The event also included on-site tattoo artists, body painting, glass blowing, kandi and craft workshops, and a hookah lounge, providing maximum entertainment for our guests. With over 15,000 attendees, we are very proud to say DTRH 3 was a huge success!</p>
-                                   <p>Now, WOA is working to expand its global and cultural influence by establishing a network of artists, promoters, and content providers in a collaborative effort to create a world comprised of the latest electronic music, news, and events.</p>
-                               </div>
-                               <div class="section">
-                                   <h3>Vision</h3>
-                                   <p>To become the premiere outlet for electronic music and events while producing state-of-the-art festivals featuring world-class headliners.</p>
-                               </div>
+                              <div class="section">
+                                  <h1 class="title">Vision</h1>
+                                 <p>To become the premiere outlet for electronic music and events while producing state-of-the-art festivals featuring world-class headliners.</p>
+                              </div>
+                              <div class="shadow"></div>
+                              <div class="section pt5">
+                                  <h1 class="title">What is the World of Anarchy?</h1>
+                                 <p>Founded in 2008, World of Anarchy began as an event production company, dedicated to the growth of electronic music on a global scale.  By teaming up with industry leaders and charities like the AVON Walk for Breast Cancer and the Costanoan Ohlone Indian Research non-profit, WOA was able to take its first steps by producing community-benefitting events.</p>
+                                 <p>In August of 2009, World of Anarchy made their presence known with one of the most memorable events of the year: “Down The Rabbit Hole 2”.  An event that started off as a small idea rapidly grew into a 3 stage, 16 hour, 3,000 person festival, on a Federal Native American Trust Land.  The event fused Native American culture with electronic music culture, and left those that attended in sheer awe!  Those lucky enough to attend still enthusiastically rave about the event to this day.</p>
+                                 <p>In early 2010, plans for the “Return of the Hatter: Down the Rabbit Hole 3” were being hatched.  In August of 2011, WOA brought DTRH back to life at the Monterey County Fair Grounds.  DTRH 3 recreated the Native American - EDM fusion, only on a much larger scale; featuring 6 Massive stages, on-site camping, over 70 renowned DJs from all over the world, and 36 hours of non-stop music. The event also included on-site tattoo artists, body painting, glass blowing, kandi and craft workshops, and a hookah lounge, providing maximum entertainment for our guests. With over 15,000 attendees, we are very proud to say DTRH 3 was a huge success!</p>
+                                 <p>Now, WOA is working to expand its global and cultural influence by establishing a network of artists, promoters, and content providers in a collaborative effort to create a world comprised of the latest electronic music, news, and events.</p>
+                              </div>
                            </div>
                            <div class="content hidden" data-sub-page="visionaries">
                                <h1>Santosh & bio, Zak & bio</h1>
