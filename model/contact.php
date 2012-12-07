26aadd8ac88d0c03fa9f9515af39edc0675a3145
Zak Fisher <zak.fisher@gmail.com>
2012-12-07 13:10:34 -0800

    cleaned up contact email

--- model/contact.php
+++ model/contact.php
@@ -15,7 +15,14 @@ class Contact_Model extends WOA {
       foreach($assoc_array as $value) $text->sanitize_string($value);
       $db->insert_into('emails', $assoc_array);
 
+      $msg =
+         "<p>Sender: "  . $assoc_array['name'] . "</p>
+          <p>Email: "   . $assoc_array['email'] . "</p>
+          <p>Subject: " . $assoc_array['subject'] . "</p>
+          <p>Message: " . $assoc_array['message'] . "</p>
+      ";
+
       // Send Email
-      $email->send('info@worldofanarchy.com', 'WOA Visitor: ' . $assoc_array['name'], $assoc_array['subject'], $assoc_array['message']);
+      $email->send('info@worldofanarchy.com', 'World of Anarchy', 'Visitor Email', $msg);
    }
 }
