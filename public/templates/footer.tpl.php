83d465ef1b01e568e4efbff53b81447bebb7779d
Zak Fisher <zak.fisher@gmail.com>
2013-12-08 09:52:38 -0800

    added google analytics

--- public/templates/footer.tpl.php
+++ public/templates/footer.tpl.php
@@ -13,6 +13,18 @@
     <img id="loading" src="/images/loading.gif" />
     <div id="overlay"></div>
     <?php if ($this->isLoggedIn): ?><input id="is-logged-in" type="hidden" /><?php endif; ?>
+    <?php if (ENV === 'production'): ?>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-46301513-1', 'edmcake.com');
+            ga('send', 'pageview');
+
+        </script>
+    <?php endif; ?>
 </body>
 <script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
